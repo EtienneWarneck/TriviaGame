27568da015c2d1a5b134ae48c2ef1e480c95e130
Etienne Warneck <etienne.warneck@gmail.com>
2019-10-13 11:07:07 -0400

    commiting last version test

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,87 +1,62 @@
 
-var array1 = [
-    {
-        question: "Question 1 of 3: Which of these 3 artists made this drawing in 1912 ?",
-        image: { i: "" },
-        answers: {
-            a: "a. Fernand Léger",
-            b: "b. Pablo Picasso",
-            c: "c. Juan Gris",
-        },
-        correctAnswer: {
-            correct: "b. Pablo Picasso, Guitar 1912"
-        },
-    },
-];
-    var array2 = [
-        {
-        question: "What artist made this artwork?",
-        image: { i: "img_02.png" },
-        answers: {
-            a: "a. Roy Lichtenstein",
-            b: "b. James Rosenquist",
-            c: "c. Claus Oldenburg",
-        },
-        correctAnswer: {
-            correct: "b. Pablo Picasso, Guitar 1912"
-        },
-    },
-]
+var array1 = ["Question 1 of 3: Which of these 3 artists made this drawing in 1912 ?",
+    "a. Fernand Léger", "b. Pablo Picasso", "c. Juan Gris"];
+// correctAnswer: [1]
 
-var results = ["Congratulations!Good answer", "Nope", "Time's up!"]
+var array2 = ["Question 2 of 3: Which of these 3 artists made this painting in 1964 ?",
+    "a. Roy Lichtenstein", "b. James Rosenquist", "c. Claus Oldenburg"];
+// correctAnswer: 2
 
-var nextQuestion;
-var clockRunning = false;
-var time = 0;
-var number = 100;
-var progressBar;
+var results = ["Congratulations!Good answer", "Nope, try again", "Time's up!"];
 
-window.onload = function () {
-    $("#buttonStart").on("click", startQuiz);
-    $("#buttonStart").on("click", move);
+onload = function () {
+    $("#buttonStart").on("click", question1); //START BUTTON CALLING FIRST QUESTION
+    $("#buttonStart").on("click", move); //START BUTTON CALLING PROGRESS BAR
 };
 
-function startQuiz() {
-    $("#question").html("<h4>" + array1[0].question + "</h4>");
-    $("#answer1").html("<h4>" + array1[0].answers.a + "</h4>");
-    $("#answer2").html("<h4>" + array1[0].answers.b + "</h4>");
-    $("#answer3").html("<h4>" + array1[0].answers.c + "</h4>");
-    $("#image").html("<h4>" + array1[0].image.i + "<img src='assets/images/img_01.png' width='400px'>");
+function question1() {
+    $("#question").html("<h4>" + array1[0] + "</h4>");
+    $("#answer1").html("<h4>" + array1[1] + "</h4>");
+    $("#answer2").html("<h4>" + array1[2] + "</h4>");
+    $("#answer3").html("<h4>" + array1[3] + "</h4>");
+    $("#image").html("<img src='assets/images/img_01.png' width='400px'>");
     $(".answ").css("visibility", "visible");
     move();
+    $("#buttonContinue").css("visibility", "visible");
 };
 
+$("#buttonContinue").on("click", question2()) //NOT WORKING
+
+//Trying to write if answer 2 of array 1 is clicked, show results[0], else show results[1]
+// ($("#answer2").results.html("<h4>" + results[0] + "</h4>")
+
+function question2() {
+    $("#question").html("<h4>" + array2[0] + "</h4>");
+    $("#answer1").html("<h4>" + array2[1] + "</h4>");
+    $("#answer2").html("<h4>" + array2[1] + "</h4>");
+    $("#answer3").html("<h4>" + array2[1] + "</h4>");
+    $("#image").html("<img src='assets/images/img_02.png width='400px'>");
+    $(".answ").css("visibility", "visible");
+    move();
+}
 
 
-// function move() {  //FOUND CODE
-//     $("#myProgress").css("visibility", "visible");
-//     var elem = $("#myBar");
-//     var width = 1;
-//     var id = setInterval(frame, 10);
-//     function frame() {
-//         if (width === 100) {
-//             clearInterval(id);
-//         } else {
-//             width++;
-//             elem.style.width = width + '%';
-//         }
-//     }
-// }
- function move() {
+function move() {  //PROGRESS BAR
     $("#myProgress").css("visibility", "visible");
-  var elem = document.getElementById("myBar");   
-  var width = 1;
-  var id = setInterval(frame, 100);
-  function frame() {
-    if (width >= 100) {
-      clearInterval(id);
-    } else {
-      width++; 
-      elem.style.width = width + '%'; 
+    var elem = document.getElementById("myBar");
+    var width = 1;
+    var id = setInterval(frame, 100);
+    function frame() {
+        if (width >= 100) {
+            clearInterval(id);
+        } else {
+            width++;
+            elem.style.width = width + '%';
+        }
     }
-  }
-} 
+}
 
+//HOVER ANSWERS IN COLOR YELLOW
 $(function () { //same as jQuery(document).ready(function(){});
 
     $('li').css('background-color', 'white').hover(function () {
@@ -90,9 +65,9 @@ $(function () { //same as jQuery(document).ready(function(){});
         function () {
             $(this).css('background-color', 'white');
         });
-    // $("p").mouseout(function(){
-    //   $("p").css("background-color", "lightgray");
-    // });
+    $("p").mouseout(function () {
+        $("p").css("background-color", "lightgray");
+    });
 });
 
 
